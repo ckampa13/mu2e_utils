fc44904a15aab4452265ebab9bd9c7a66f6a0605
Cole Kampa <ckampa13@gmail.com>
2021-02-11 19:48:41 -0600

    A few jupyter notebooks beginning to work through stats papers.

---
+++ stats/notebooks/plot_config.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def config_plots():
+    #must run twice for some reason (glitch in Jupyter)
+    for i in range(2):
+        plt.rcParams['figure.figsize'] = [10, 8] # larger figures
+        plt.rcParams['axes.grid'] = True         # turn grid lines on
+        plt.rcParams['axes.axisbelow'] = True    # put grid below points
+        plt.rcParams['grid.linestyle'] = '--'    # dashed grid
+        plt.rcParams.update({'font.size': 12.0})   # increase plot font size
